cdd9ae1babf9c051b336aa7aaf38c1f8639cee91
Qmnia Arafat <83679206+Omnia-Arafat@users.noreply.github.com>
2023-04-11 07:27:31 +0200

    adding log in page

---
+++ 1st_trail/client/components/pages/log_in.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import"../pages/log_in.css";
+
+const log_in = () => {
+  return (
+    <div>
+    <div class="container">
+    <h1 id="welcome-text">Welcome , Omnia</h1>
+    <div class="main-content">
+      <input type="email" id="email" placeholder="Please Enter Your Email" />
+      <input
+        type="password"
+        id="password"
+        placeholder="Please Enter Your password"
+      />
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default log_in
+
+
+
+
+
--- 1st_trail/client/src/App.js
+++ 1st_trail/client/src/App.js
@@ -19,25 +19,7 @@ function App() {
 
   return (
     <div>
-      <h1>User List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      
     </div>
   );
 }
