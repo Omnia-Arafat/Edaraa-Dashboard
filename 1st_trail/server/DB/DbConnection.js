81d7b962cd229f12c31c6d2a683872851b5316e9
Qmnia Arafat <83679206+Omnia-Arafat@users.noreply.github.com>
2023-04-11 05:05:11 +0200

    this a reorder for database and adding uploaded

---
+++ 1st_trail/server/DB/DbConnection.js
@@ -0,0 +1,29 @@
+//============== import the database =============================
+
+const mysql = require("mysql");
+
+//============== connect to the database =============================
+
+const connection = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password: 'root123',
+    port:"3306",
+    database:"cruddatabase"
+    
+     });
+    
+    connection.connect((err)=>{
+    
+    if (err) {
+    console.error("connection error"); return;
+    }
+    
+    
+    console.log("Connected to mysql");
+    
+    });
+    
+    module.exports = connection;
+    
--- 1st_trail/server/index.js
+++ 1st_trail/server/index.js
@@ -1,77 +1,51 @@
-const express = require("express")
-const app =express();
-const mysql = require("mysql");
+// =================== INITIALIZE EXPRESS APP ========================
 
-const path = require('path');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const app = express();
+
+// =================== GLOBAL MIDDLEWARE ========================
+
+const db = require("../server/DB/DbConnection");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
+app.use(cors());
 const bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended:true}))
-
-//connect to the database
-
-const db = mysql.createConnection({
-
-host: 'localhost',
-user: 'root',
-password: 'root123',
-port:"3306",
-database:"cruddatabase"
-
- });
-
-db.connect((err)=>{
-
-if (err) {
-console.error("connection error"); return;
-}
-
-
-console.log("Connected to mysql");
-
-});
-
-module.exports = db;
-
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // execute the INSERT query
-db.query("INSERT INTO user (usertype, Email, password,phone,state) VALUES ('admin', 'Esraa shapan.a@gmail.com', 'mypassword','01111111','0');", (err, results, fields) => {
+db.query(
+  "INSERT INTO user (usertype, Email, password,phone,state) VALUES ('admin', 'Esraa shapan.a@gmail.com', 'mypassword','01111111','0');",
+  (err, results, fields) => {
     if (err) {
-      console.error('Error executing query:', err);
+      console.error("Error executing query:", err);
       return;
     }
-    console.log('Query executed successfully');
-  });
-  
-  // close the connection to the database
-  db.end();
+    console.log("Query executed successfully");
+  }
+);
 
+// close the connection to the database
+db.end();
 
-  app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-  
+// Your API routes go here
 
-  // Your API routes go here
-  
-  app.get('/api/users', function(req, res) {
-    const users = [
-      { id: 1, name: 'Alice' },
-      { id: 2, name: 'Bob' },
-      { id: 3, name: 'Charlie' }
-    ];
-    console.log('Sending data:', users); // Log the data being sent to the console
-    res.json(users);
-    
-  });
-  
-   
-    
- 
-app.listen("5000",()=>{
+app.get("/api/users", function (req, res) {
+  const users = [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+    { id: 3, name: "Charlie" },
+  ];
+  console.log("Sending data:", users); // Log the data being sent to the console
+  res.json(users);
+});
 
-console.log("run in port 5000")
+// =================== RUN THE APP ========================
 
-})
+app.listen("5000", () => {
+  console.log("run in port 5000");
+});
