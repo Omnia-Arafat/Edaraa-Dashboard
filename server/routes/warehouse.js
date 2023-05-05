b10d3454ccc6915c65d3eb70416134ea599e767e
NourSewafy11 <118844362+NourSewafy11@users.noreply.github.com>
2023-05-05 15:34:50 +0300

    update warehous pages

--- server/routes/warehouse.js
+++ server/routes/warehouse.js
@@ -6,9 +6,10 @@ const { body, validationResult } = require("express-validator");
 const upload = require("../midleware/uploadimage");
 const util = require("util"); // helper
 const fs = require("fs"); // file system
-creat new warehouse(admin)
+
+//creat new warehouse(admin)
 router.post(
-    "",
+    "/create",admin,
    // upload.single("image"),
     body("name")
       .isString()
@@ -22,21 +23,19 @@ router.post(
       //.isLength({ min: 20 }) 
       //.withMessage("description name should be at lease 20 characters"),
       body("status")
-      .isLength({ min: 20 })
+      .isLength({ min: 1 })
       .withMessage("enter warehouose is active or not ...0=>inactive/1=>active"),
     async (req, res) => {
       try {
         // 1- VALIDATION REQUEST [manual, express validation]
         const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        res.status(200).json({
-            msg :"warehouse created",
-        });
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
   
         // 2- VALIDATE THE IMAGE
-        if (!req.file) {
+       /* if (!req.file) {
           return res.status(400).json({
             errors: [
               {
@@ -44,38 +43,83 @@ router.post(
               },
             ],
           });
-        }
+        }*/
   
-        /*// 3- PREPARE MOVIE OBJECT
-        const movie = {
+        // 3- PREPARE MOVIE OBJECT
+        const warehouse = {
           name: req.body.name,
-          description: req.body.description,
-          image_url: req.file.filename,
+          location: req.body.location,
+          status: req.body.status,
         };
   
         // 4 - INSERT MOVIE INTO DB
         const query = util.promisify(conn.query).bind(conn);
-        await query("insert into movies set ? ", movie);
+        await query("insert into warehouse set ? ", warehouse);
         res.status(200).json({
-          msg: "movie created successfully !",
+          msg: "warehouse created successfully !",
         });
         //console.log(err);
       } catch (err) {
         res.status(500).json(err);
       }
     }
-  );*/
-  router.post("/create", (req, res) => {
-    res.status(200).json({
-      msg : "warehouse created",
-    });
-  });
+  );
+ 
 
-  router.put("/updated",(req,res) => {
-    res.status(200).json({
-      msg : "warehouse updated",
-    });
-  });
+  router.put(
+    "/:id", // params
+    admin,
+    upload.single("image"),
+    body("name")
+      .isString()
+      .withMessage("please enter a valid movie name")
+      .isLength({ min: 10 })
+      .withMessage("movie name should be at lease 10 characters"),
+  
+    body("description")
+      .isString()
+      .withMessage("please enter a valid description ")
+      .isLength({ min: 20 })
+      .withMessage("description name should be at lease 20 characters"),
+    async (req, res) => {
+      try {
+        // 1- VALIDATION REQUEST [manual, express validation]
+        const query = util.promisify(conn.query).bind(conn);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+  
+        // 2- CHECK IF MOVIE EXISTS OR NOT
+        const movie = await query("select * from movies where id = ?", [
+          req.params.id,
+        ]);
+        if (!movie[0]) {
+          res.status(404).json({ ms: "movie not found !" });
+        }
+  
+        // 3- PREPARE MOVIE OBJECT
+        const movieObj = {
+          name: req.body.name,
+          description: req.body.description,
+        };
+  
+        if (req.file) {
+          movieObj.image_url = req.file.filename;
+          fs.unlinkSync("./upload/" + movie[0].image_url); // delete old image
+        }
+  
+        // 4- UPDATE MOVIE
+        await query("update movies set ? where id = ?", [movieObj, movie[0].id]);
+  
+        res.status(200).json({
+          msg: "movie updated successfully",
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
+  );
 
     router.delete("/delete",(req,res) => {
     res.status(200).json({
