8caa3de994adaedc1ed07afe4b26883226c8f8b8
NourSewafy11 <118844362+NourSewafy11@users.noreply.github.com>
2023-05-08 16:53:06 +0300

    add request page

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require("express");
 const app = express();
 const db = require("../server/DB/DbConnection");
@@ -35,18 +36,41 @@ app.get('/product', (req, res) => {
 // });
 
 // ====================  Required Module ====================
+=======
+
+// ==================== INITIALIZE EXPRESS APP ====================
+const express = require("express");
+const app = express();
+
+// ====================  GLOBAL MIDDLEWARE ====================
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // TO ACCESS URL FORM ENCODED
+app.use(express.static("upload"));
+const cors = require("cors");
+app.use(cors()); // ALLOW HTTP REQUESTS LOCAL HOSTS
+
+// ====================  Required Module ====================
+
+>>>>>>> Stashed changes
 const auth = require("./routes/auth");
 const warehouse = require("./routes/warehouse");
 const product = require("./routes/product");
+const request = require("./routes/request");
 const manage_supervisor = require("./routes/manage_supervisor");
 
 // ====================  API ROUTES [ ENDPOINTS ]  ====================
 app.use("/auth", auth);
 app.use("/warehouse", warehouse);
 app.use("/product", product);
+<<<<<<< Updated upstream
 app.use("/manage_supervisor", manage_supervisor);
 
 // =================== RUN THE APP ========================
 app.listen("5000", () => {
   console.log("run in port 5000");
 });
+=======
+app.use("/request", request);
+
+app.use("/manage_supervisor", manage_supervisor);
+>>>>>>> Stashed changes
--- server/routes/Request.js
+++ server/routes/Request.js
@@ -3,168 +3,72 @@ const conn = require("../DB/DbConnection");
 // const authorized = require("../middleware/authorize");
 const admin = require("../middleware/admin");
 const { body, validationResult } = require("express-validator");
-const upload = require("../middleware/uploadImages");
+//const upload = require("../middleware/uploadImages");
 const util = require("util"); // helper
 const fs = require("fs"); // file system
 
 // CREATE MOVIE [ADMIN]
+
+ 
+
 router.post(
   "",
-  supervisor,
-
-  body("name")
+    
+   // upload.single("image"),
+   body("users_id").isNumeric().withMessage("please enter   your id"),
+  body("product_id").isNumeric().withMessage("please enter product_id"),
+  body("request")
     .isString()
-    .withMessage("please enter a valid Request name")
+    .withMessage("please enter a valid request ")
     .isLength({ min: 10 })
-    .withMessage("movie name should be at lease 10 characters"),
-
-  body("description")
-    .isString()
-    .withMessage("please enter a valid description ")
-    .isLength({ min: 20 })
-    .withMessage("description name should be at lease 20 characters"),
-  async (req, res) => {
-    try {
-      // 1- VALIDATION REQUEST [manual, express validation]
-      const errors = validationResult(req);
+    .withMessage("request name should be at lease 20 characters"),
+   async (req, res) => {
+      try {
+        // 1- VALIDATION REQUEST [manual, express validation]
+        const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
-
-     
-
-      // 3- PREPARE MOVIE OBJECT
-      const movie = {
-        name: req.body.name,
-        description: req.body.description,
-        image_url: req.file.filename,
-      };
-
-      // 4 - INSERT MOVIE INTO DB
-      const query = util.promisify(conn.query).bind(conn);
-      await query("insert into movies set ? ", movie);
-      res.status(200).json({
-        msg: "movie created successfully !",
-      });
-    } catch (err) {
-      res.status(500).json(err);
+        // 3- PREPARE MOVIE OBJECT
+        const request = {
+          users_id: req.body.users_id,
+        product_id: req.body.product_id,
+        request: req.body.request,
+        };
+  
+        // 4 - INSERT MOVIE INTO DB
+        const query = util.promisify(conn.query).bind(conn);
+        await query("insert into request set ? ", request);
+        res.status(200).json({
+          msg: "request created successfully !",
+        });
+        //console.log(err);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
     }
-  }
-);
+  );
 
-// UPDATE MOVIE [ADMIN]
-// router.put(
-//   "/:id", // params
-//   admin,
-//   upload.single("image"),
-//   body("name")
-//     .isString()
-//     .withMessage("please enter a valid movie name")
-//     .isLength({ min: 10 })
-//     .withMessage("movie name should be at lease 10 characters"),
 
-//   body("description")
-//     .isString()
-//     .withMessage("please enter a valid description ")
-//     .isLength({ min: 20 })
-//     .withMessage("description name should be at lease 20 characters"),
-//   async (req, res) => {
-//     try {
-//       // 1- VALIDATION REQUEST [manual, express validation]
-//       const query = util.promisify(conn.query).bind(conn);
-//       const errors = validationResult(req);
-//       if (!errors.isEmpty()) {
-//         return res.status(400).json({ errors: errors.array() });
-//       }
 
-//       // 2- CHECK IF MOVIE EXISTS OR NOT
-//       const movie = await query("select * from movies where id = ?", [
-//         req.params.id,
-//       ]);
-//       if (!movie[0]) {
-//         res.status(404).json({ ms: "movie not found !" });
-//       }
+router.get("", async (req, res) => {
+  const query = util.promisify(conn.query).bind(conn);
+  const request = await query("select * from request ");
+ 
+  
+  res.status(200).json(request);
+});
 
-//       // 3- PREPARE MOVIE OBJECT
-//       const movieObj = {
-//         name: req.body.name,
-//         description: req.body.description,
-//       };
+router.get("/:users_id", async (req, res) => {
+  const query = util.promisify(conn.query).bind(conn);
+  const request = await query("select * from request where users_id = ?", [
+    req.params.users_id,
+  ]); 
+  
+  res.status(200).json(request);
+});
 
-//       if (req.file) {
-//         movieObj.image_url = req.file.filename;
-//         fs.unlinkSync("./upload/" + movie[0].image_url); // delete old image
-//       }
-
-//       // 4- UPDATE MOVIE
-//       await query("update movies set ? where id = ?", [movieObj, movie[0].id]);
-
-//       res.status(200).json({
-//         msg: "movie updated successfully",
-//       });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   }
-// );
-
-// // DELETE MOVIE [ADMIN]
-// router.delete(
-//   "/:id", // params
-//   admin,
-//   async (req, res) => {
-//     try {
-//       // 1- CHECK IF MOVIE EXISTS OR NOT
-//       const query = util.promisify(conn.query).bind(conn);
-//       const movie = await query("select * from movies where id = ?", [
-//         req.params.id,
-//       ]);
-//       if (!movie[0]) {
-//         res.status(404).json({ ms: "movie not found !" });
-//       }
-//       // 2- REMOVE MOVIE IMAGE
-//       fs.unlinkSync("./upload/" + movie[0].image_url); // delete old image
-//       await query("delete from movies where id = ?", [movie[0].id]);
-//       res.status(200).json({
-//         msg: "movie delete successfully",
-//       });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   }
-// );
-
-// LIST & SEARCH [ADMIN, USER]
-// router.get("", async (req, res) => {
-//   const query = util.promisify(conn.query).bind(conn);
-//   let search = "";
-//   if (req.query.search) {
-//     // QUERY PARAMS
-//     search = `where name LIKE '%${req.query.search}%' or description LIKE '%${req.query.search}%'`;
-//   }
-//   const movies = await query(`select * from movies ${search}`);
-//   movies.map((movie) => {
-//     movie.image_url = "http://" + req.hostname + ":4000/" + movie.image_url;
-//   });
-//   res.status(200).json(movies);
-// });
-
-// // SHOW MOVIE [ADMIN, USER]
-// router.get("/:id", async (req, res) => {
-//   const query = util.promisify(conn.query).bind(conn);
-//   const movie = await query("select * from movies where id = ?", [
-//     req.params.id,
-//   ]);
-//   if (!movie[0]) {
-//     res.status(404).json({ ms: "movie not found !" });
-//   }
-//   movie[0].image_url = "http://" + req.hostname + ":4000/" + movie[0].image_url;
-//   movie[0].reviews = await query(
-//     "select * from user_movie_review where movie_id = ?",
-//     movie[0].id
-//   );
-//   res.status(200).json(movie[0]);
-// });
 
 // MAKE REVIEW [ADMIN, USER]
 // router.post(
